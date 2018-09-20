8658f1ff72b2fc0b37100d1c6b9b38de5122c1b0
Walze <catufuzgu@gmail.com>
2018-09-20 11:02:17 -0300

    removed deploy, fixed barrel

--- src/barrel.js
+++ src/barrel.js
@@ -1,4 +1,7 @@
 ///<reference path="./index.d.ts"/>
 
-export * from './js/**.js'
+export * from './js/FileRenderer.js'
+export * from './js/FileFormatter.js'
+export * from './js/StringFormatter.js'
+export * from './js/helpers.js'
 
