c2ffc35139758fbe635ce16d59a20c7366333a11
Walze <catufuzgu@gmail.com>
2018-10-05 14:39:13 -0300

    adding template

--- public/textos/model.md
+++ public/textos/model.md
@@ -1,6 +1,26 @@
 <<model>>
 
+
 asd1
+
 asd2
+
 asd3
+asdf4
+asdf5
+adsf6
+
+asdf47
+
+
+
+asd1
+
+asd2
+
+asd3
+asdf
+asdf
+adsf
+
 asdf4
--- public/triggers.ts
+++ public/triggers.ts
@@ -4,6 +4,46 @@ import "@babel/polyfill"
 
 import { ITriggerType } from "../src/types"
 
+interface Iloop<A> {
+  index: number;
+  dimention: number;
+  dimentionIndex: number;
+  arr: A[] | A[][];
+}
+
+const multidimentionLoop = <A>(
+  arr: A[] | A[][],
+  func: (value: A, obj: Iloop<A>) => void,
+  objRef?: Iloop<A>,
+) => {
+
+  const obj = objRef || {
+    index: 0,
+    dimention: 0,
+    dimentionIndex: -1,
+    arr,
+  }
+
+  obj.dimentionIndex = -1
+
+  for (const item of arr) {
+    obj.dimentionIndex++
+
+    if (!Array.isArray(item)) {
+      func(item as A, obj)
+      obj.index++
+      continue
+    }
+
+    const arr2 = item as A[] | A[][]
+
+
+    multidimentionLoop(arr2, func, obj)
+  }
+
+  obj.dimention++
+}
+
 export const triggers: ITriggerType = {
   default: (...args) => console.info('Default Triggered', args),
 
@@ -30,26 +70,28 @@ export const triggers: ITriggerType = {
 
   model(ref, { data }, divs) {
 
-    const model = divs[0].querySelector('[for-model]')
-    if (!model) return
+    const broken = ref
+      .everyNthLineBreak(data, 3)
+      .map((string) =>
+        ref.everyNthLineBreak(string, 1)
+          .map((item, i) =>
+            i === 2
+              ? ref.everyNthLineBreak(item, 0)
+              : item,
+          ),
+      )
 
-    model.innerHTML = model.innerHTML
-      .replace(
-        /(<[^>]+>)([^<\/]+)?(<\/[^>]+>)/g,
-        (_, open, text, close) => {
+    console.log(broken)
 
-          console.warn(open, text, close)
+    const models = Array.from(divs[0].querySelectorAll('[for-model]'))
+    if (!models) return
 
-          text = data
 
-          return `${open}${text}${close}`
+    multidimentionLoop(broken, (_, obj) => {
 
-        })
+      console.log(obj, obj.arr)
 
-    console.log([
-      ref,
-      data,
-    ])
+    })
 
   },
 
